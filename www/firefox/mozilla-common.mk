54bbab469c1e2e5e0ea9e4f77be2db7ea534c4cf
Michael Forney <mforney@mforney.org>
2020-01-22 20:29:14 -0800

    firefox: use bsdtar on all platforms

--- www/firefox/mozilla-common.mk
+++ www/firefox/mozilla-common.mk
@@ -76,12 +76,8 @@ do-test:
 	     ./run-mozilla.sh ${WRKSRC}/mach check-spidermonkey
 
 # tar(1) of OpenBSD 5.5 has no --exclude command line option.
-.if ${OPSYS} == "OpenBSD"
 TOOLS_PLATFORM.tar=	${TOOLS_PATH.bsdtar}
 USE_TOOLS+=		bsdtar
-.else
-USE_TOOLS+=		tar
-.endif
 
 .if ${MACHINE_ARCH} == "i386"
 # This is required for SSE2 code under i386.
